bfc8f72911dbef52ab1de7ac0fcdf225910360e4
whinc <whincwu@163.com>
2018-05-08 01:36:04 +0800

    feat(network): 初步实现对 XMLHttpRequest 的请求的拦截显示

--- src/network/NetworkPanel.vue
+++ src/network/NetworkPanel.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="network-panel">
     <div class="table">
-      <div class="row" v-for="i in 10" :key="i">
+      <div class="row">
         <span class="cell long" :style="{'max-width': `${4/6*100}vw`}">Name</span>
         <span class="cell">Method</span>
         <span class="cell">Status</span>
+      </div>
+      <div class="row" v-for="item in requestList" :key="item.id">
+        <span class="cell long" :style="{'max-width': `${4/6*100}vw`}">{{item.url}}</span>
+        <span class="cell">{{item.method}}</span>
+        <span class="cell">{{item.status}}</span>
       </div>
     </div>
     <div class="message-list">
@@ -28,12 +33,87 @@ export default {
     TabItem,
     TabBar
   },
+  data () {
+    return {
+      requestList: {}
+    }
+  },
+  mounted () {
+    this.hookXMLHttpRequest()
+
+    var xhr = new window.XMLHttpRequest()
+    xhr.open('GET', 'https://whinc.github.io/web-console/')
+    xhr.send()
+  },
   methods: {
     onClickClear() {
 
     },
     onClickHide () {
       this.$root.$emit('hide')
+    },
+    /**
+     * 拦截 XMLHttpRequest 请求并记录状态
+     * 
+     * 参考：<http://javascript.ruanyifeng.com/bom/ajax.html>
+     */
+    hookXMLHttpRequest () {
+      const vm = this
+      const _open = window.XMLHttpRequest.prototype.open
+      const _send = window.XMLHttpRequest.prototype.send
+
+      window.XMLHttpRequest.prototype.open = function (method, url) {
+        const xhr = this
+        const id = vm.getUniqueID()
+
+        xhr.$id = id
+        xhr.$method = method
+        xhr.$url = url
+
+        const _onreadystatechange = xhr.onreadystatechange || function() {};
+        xhr.onreadystatechange = function () {
+          const item = vm.requestList[id] || {}
+
+          item.readyState = xhr.readyState
+          item.status = 0
+          if (item.readyState >= 2) {
+            item.status = xhr.status
+          }
+
+          vm.updateRequest(id, item)
+
+          _onreadystatechange.apply(this, arguments)
+        }
+
+        _open.apply(this, arguments)
+      }
+
+
+      window.XMLHttpRequest.prototype.send = function (...args) {
+        const xhr = this
+        const item = vm.requestList[xhr.$id] || {}
+        item.id = xhr.$id
+        item.method = xhr.$method
+        item.url = xhr.$url
+
+        _send.apply(this, arguments)
+      }
+    },
+    getUniqueID() {
+      let id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8)
+          return v.toString(16)
+      })
+      return id
+    },
+    updateRequest (id, item) {
+      const _item = this.requestList[id]
+      if(!_item) {
+        this.$set(this.requestList, id, item)
+        return
+      }
+
+      Object.keys(item).forEach(key => _item[key] = item[key])
     }
   } 
 }
