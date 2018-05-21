a0282a4d4a861c708774b0f8a07acd7ad15a7020
whincwu <whincwu@163.com>
2018-05-22 00:20:08 +0800

    feat: 导出全局的 WebConsole 变量

--- src/main.js
+++ src/main.js
@@ -3,13 +3,37 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-window.addEventListener('DOMContentLoaded', function () {
-  const root = document.createElement('div')
-  document.body.appendChild(root)
+let isInit = false
+const load = () => {
+  if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    const root = document.createElement('div')
+    document.body.appendChild(root)
 
-  new Vue({
-    el: root,
-    render: h => h(App)
-  })
+    new Vue({
+      el: root,
+      render: h => h(App)
+    })
+  } else {
+    document.addEventListener('readystatechange', load)
+  }
+}
 
-})
+class WebConsole {
+  static init () {
+    if (isInit) {
+      console.warn('WebConsole can only be initialize once')
+      return
+    }
+
+    load()
+    isInit = true
+  }
+}
+
+if (window && !window.WebConsole) {
+  window.WebConsole = WebConsole
+} else {
+  console.warn('window.WebConsole is not empty')
+}
+
+export default WebConsole
