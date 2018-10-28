4df4f8c2896976ce2950294581c71459b4be57c7
whincwu <whincwu@163.com>
2018-10-28 23:09:51 +0800

    feat(application): 增加cookie展示

--- src/application/TabCookie.vue
+++ src/application/TabCookie.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Cookie</div>  
+  <div>
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Value</th>
+      </thead>
+      <tbody>
+        <tr v-for="cookie in cookieList" :key="cookie.name">
+          <td>{{cookie.name}}</td>
+          <td>{{cookie.value}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>  
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 数组元素数据结构: {name: string, value: any}
+      cookieList: []
+    };
+  },
+  mounted() {
+    const cookie = document.cookie;
+    if (cookie) {
+      const kvList = cookie.split(/\s*;\s*/);
+      const cookieList = kvList.map(v => {
+        const arr = v.split("=");
+        return {
+          name: arr[0],
+          value: arr[1]
+        };
+      });
+      this.cookieList = cookieList;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
