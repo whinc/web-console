f89747de9e3b37ccd6b82d0c2865212839e99551
whincwu <whincwu@webank.com>
2018-10-30 19:52:55 +0800

    Updates

--- test_console.js
+++ test_console.js
@@ -5,10 +5,210 @@ window.$console = (function() {
     console.log(format, value);
   }
 
-  function testException() {
-    throw new Error("xxx");
+  function testIntervalLog(interval = 1000) {
+    console.log(Date.now());
+    setTimeout(testIntervalLog.bind(null, arguments), interval);
   }
 
+  function testException() {
+    // console.log(new Error('general error'))
+    // console.error(new Error('general error'))
+    console.error(new ReferenceError("reference error"));
+    // throw new Error("xxx");
+  }
+
+  // 打印对象
+  function testObject() {
+    // console.log('obj:', {a: 1})
+    // break
+    // console.log('this is a object: ', obj, 'wa haha')
+    // console.log({
+    //   c: 3,
+    //   b: 2,
+    //   a: 1
+    // })
+    // break
+    let obj2 = {
+      // 测试数值类型
+      a: 1,
+      a5: { a: 1, b: 2, c: { a: 1, b: 2 } },
+      // 测试数组
+      a3: [1, 2],
+      a4: [1, 2, [1, 2, [1, 2, 3], 3], 3],
+      // 测试属性值很长时的展示
+      a1: "10101010101010101010101010101010101010101010101001010101",
+      a2: 10101010101010101010110101010101010110101011010101011010101,
+      // 测试字符串类型
+      b: "b",
+      // 测试转义字符展示
+      b1: '"b1"',
+      c: true,
+      d: undefined,
+      e: null,
+      f: Symbol(),
+      g: {
+        a: 1,
+        f1: () => {
+          return 1;
+        },
+        f2: function() {
+          return 2;
+        },
+        b: "b",
+        c: {
+          a: 1,
+          b: "b"
+        },
+        d: undefined,
+        e: null
+      },
+      [Symbol()]: 10,
+      [Symbol("age")]: 28,
+      f1: function() {},
+      f2: () => {},
+      f3: function say() {},
+      Z: "Z",
+      M: "m",
+      z: "z",
+      // 测试排序规则
+      __a: "public __a",
+      _a: "public _a",
+      $a: "public $a"
+    };
+    Object.defineProperties(obj2, {
+      // value
+      h: {
+        enumerable: true,
+        configurable: true,
+        value: 10,
+        writable: false
+      },
+      // value
+      i: {
+        enumerable: false,
+        configurable: false,
+        value: 10,
+        writable: true
+      },
+      // getter and setter
+      j: {
+        get: () => {
+          return Math.random() + "";
+        },
+        set: v => {}
+      },
+      J: {
+        get: () => {
+          return "" + Math.random();
+        },
+        set: v => {}
+      },
+      // only getter
+      k: {
+        get: function() {
+          return {
+            a: 1,
+            b: "b"
+          };
+        }
+      },
+      K: {
+        get: function() {
+          return [
+            {
+              a: 1,
+              b: "b"
+            }
+          ];
+        }
+      },
+      // only setter
+      l: {
+        set: function(v) {}
+      },
+      [Symbol("a")]: {
+        value: "1",
+        enumerable: false
+      },
+      // 测试属性排序规则
+      __b: {
+        value: "private __b",
+        enumerable: false
+      },
+      _b: {
+        value: "private _b",
+        enumerable: false
+      },
+      $b: {
+        value: "private $b",
+        enumerable: false
+      }
+    });
+    Object.defineProperties(obj2.g, {
+      h: {
+        enumerable: true,
+        configurable: true,
+        value: 10
+      },
+      i: {
+        enumerable: false,
+        configurable: false,
+        value: 10
+      },
+      j: {
+        get: () => {
+          return Math.random();
+        },
+        set: v => {}
+      },
+      k: {
+        get: function() {
+          return Math.random();
+        },
+        set: function() {}
+      }
+    });
+    Object.defineProperties(obj2.g.c, {
+      c: {
+        enumerable: false,
+        configurable: false,
+        value: 10
+      },
+      d: {
+        get: function() {
+          return Math.random();
+        },
+        set: function() {}
+      }
+    });
+    console.log("对象全属性:", obj2);
+    console.log("三种函数:", {
+      f1: function() {},
+      f2: () => {},
+      f3: function say() {}
+    });
+    var arr = [];
+    arr["10"] = "10";
+    arr["29"] = "29";
+    arr[" "] = " "; // 0x20
+    arr["  "] = "  ";
+    arr[" a"] = " a";
+    arr["  a"] = "  a"; // 空白符处理
+    arr["!"] = "!"; // 0x21
+    arr["0"] = "0"; // 0x30
+    arr["1"] = "1"; // 0x31
+    arr["2"] = "2"; // 0x31
+    arr[":"] = ":"; // 0x3A
+    arr["A"] = "A"; // 0x41
+    arr["B"] = "B"; // 0x42
+    arr["["] = "["; // 0x5B
+    arr["a"] = "a"; // 0x61
+    arr["b"] = "b"; // 0x62
+    arr["{"] = "{"; // 0x7B
+    console.log("数组 key 排序:", arr);
+  }
+
+  // 格式化输出
   function testFormat() {
     var numInt = 1;
     var numFloat = 1.23;
@@ -55,6 +255,8 @@ window.$console = (function() {
 
   return {
     testFormat: testFormat,
-    testException: testException
+    testException: testException,
+    testIntervalLog: testIntervalLog,
+    testObject: testObject
   };
 })();
