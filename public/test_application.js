cfb9f05d732ccfb368b6a421ffb2abe23f7e8294
whincwu <whincwu@163.com>
2018-11-24 21:44:09 +0800

    test: 更新测试用例

--- public/test_application.js
+++ public/test_application.js
@@ -1,19 +1,22 @@
 window.$application = (function() {
   function testWriteCookie(n = 1) {
     for (var i = 0; i < n; ++i) {
-      document.cookie = i + "=" + Date.now();
+      const obj = { timestamp: Date.now() };
+      document.cookie = i + "=" + JSON.stringify(obj);
     }
   }
 
   function testWriteLocalStorage(n = 1) {
     for (var i = 0; i < n; ++i) {
-      window.localStorage.setItem(i, Date.now());
+      const obj = { timestamp: Date.now() };
+      window.localStorage.setItem(i, JSON.stringify(obj));
     }
   }
 
   function testWriteSessionStorage(n = 1) {
     for (var i = 0; i < n; ++i) {
-      window.sessionStorage.setItem(i, Date.now());
+      const obj = { timestamp: Date.now() };
+      window.sessionStorage.setItem(i, JSON.stringify(obj));
     }
   }
 
